dacaceedcd8b18e070b560c2cdedbd3f5c5642de
coddingbear <surfwon@naver.com>
2025-05-18 19:33:04 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) # QPlainTextEdit 추가
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout) # QHBoxLayout 추가
 from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
 
 class Calculator(QWidget):
@@ -13,16 +13,24 @@ class Calculator(QWidget):
 		self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
 		self.te1.setReadOnly(True)  # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
 
-		self.btn1=QPushButton('Message', self) # 버튼 추가
+		self.btn1=QPushButton('Message', self) # 버튼 1 추가
 		self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
+
+		self.btn2=QPushButton('Clear', self) # 버튼 2 추가
+		self.btn2.clicked.connect(self.clearMessage) # 버튼2 핸들러 함수 연결
+
+		hbox = QHBoxLayout()      # 수평 박스 레이아웃을 추가하고 버튼 1, 2 추가
+		hbox.addStretch(1)        # 공백
+		hbox.addWidget(self.btn1) # 버튼 1 배치
+		hbox.addWidget(self.btn2) # 버튼 2 배치
 
 		vbox=QVBoxLayout()        # 수직 레이아웃 위젯 생성
 		vbox.addWidget(self.te1)  # 수직 레이아웃에 텍스트 에디트 위젯 추가
-		vbox.addWidget(self.btn1) # 버튼 위치
+		#vbox.addWidget(self.btn1)
+		vbox.addLayout(hbox)      # btn1 위치에 hbox를 배치
 		vbox.addStretch(1)        # 빈 공간
 
 		self.setLayout(vbox) # 빈 공간 -버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
-
 		
 		self.setWindowTitle('Calculator')     # 윈도에 표시되는 타이틀
 		self.setWindowIcon(QIcon('icon.png')) # 윈도우 아이콘 추가
@@ -32,6 +40,9 @@ class Calculator(QWidget):
 	def activateMessage(self): # 헨들러 함수 수정: 메시지가 텍스트 에디터에 출력되도록
 		# QMessageBox.information(self, "infomation", "Button clicked!")
 		self.te1.appendPlainText("Button clicked!")
+	
+	def clearMessage(self):
+		self.te1.clear()
 
 if __name__=='__main__': # pyqt는 애플리케이션 당 1개의 QApplication이 필요함
 	app = QApplication(sys.argv) # QApplication 인스턴스 생성
